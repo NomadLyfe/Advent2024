c8c66fbf3943200ddca1954aee3b4762167ce003
Jeremy Garcia <Jeremygarcia9400@gmail.com>
2024-12-19 04:39:09 -0500

    found solution to puzzle 1

---
+++ puzzle_1_soln.py
@@ -0,0 +1,9 @@
+
+with open("puzzle_1_input.txt", "r") as file:
+    input = file.read().split("\n")
+    input.pop()
+    list_1 = sorted([int(el.split("   ")[0]) for el in input])
+    list_2 = sorted([int(el.split("   ")[1]) for el in input])
+    combined = [abs(a - b) for a, b in zip(list_1, list_2)]
+
+print(sum(combined))
