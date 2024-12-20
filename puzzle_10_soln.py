edbd897f9cc466af0deb7ec3627b93be7d3fc95f
Jeremy Garcia <Jeremygarcia9400@gmail.com>
2024-12-19 20:54:03 -0500

    solved puzzle 10

--- puzzle_10_soln.py
+++ puzzle_10_soln.py
@@ -12,18 +12,28 @@ with open("puzzle_9-10_input.txt", "r") as file:
         relevant_rules_2 = []
         for page in pages:
             relevant_rules_1.extend([rule for rule in rules_by_line if page in rule])
+        relevant_rules_1 = list(dict.fromkeys(relevant_rules_1))
         for rule in relevant_rules_1:
             b, a = rule.split("|")
             if b in pages and a in pages:
                 relevant_rules_2.append(rule)
+        relevant_rules_2 = list(dict.fromkeys(relevant_rules_2))
         update_passes = True
-        for rule in relevant_rules_2:
-            b, a = rule.split("|")
-            if pages.index(b) < pages.index(a):
-                pass
-            else:
-                update_passes = False
-                break
-        if update_passes:
+        copy_rr2 = relevant_rules_2.copy()
+        while copy_rr2:
+            for rule in copy_rr2:
+                b, a = rule.split("|")
+                b_i = pages.index(b)
+                a_i = pages.index(a)
+                if b_i < a_i:
+                    copy_rr2.remove(rule)
+                else:
+                    update_passes = False
+                    pages.remove(b)
+                    pages.insert(pages.index(a), b)
+                    copy_rr2 = relevant_rules_2.copy()
+                    break
+
+        if not update_passes:
             out.append(int(pages[mid_idx]))
     print(sum(out))
