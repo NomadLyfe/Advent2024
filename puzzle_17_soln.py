4368e6187ad47fba85ae773e8432a493513c5510
Jeremy Garcia <Jeremygarcia9400@gmail.com>
2025-01-04 08:01:30 -0500

    in process of puzzle 17

---
+++ puzzle_17_soln.py
@@ -0,0 +1,25 @@
+# Read and process input
+with open("puzzle_17-18_input.txt", "r") as file:
+    input_data = file.read().strip()
+    modified_input = [(input_data[i], input_data[i+1]) if i != len(input_data)-1 else (input_data[i], None) for i in range(0, len(input_data), 2)]
+memory = ""
+for i, (file, empty) in enumerate(modified_input):
+    memory += f' {str(i)} ' * int(file)
+    if empty:
+        memory += " . " * int(empty)
+memory_positions = memory.strip().split(" ")
+memory_positions = [x for x in memory_positions if x != ""]
+reversed_memory_positions = memory_positions[::-1]
+for pos, pos_val in enumerate(reversed_memory_positions):
+    pos = len(memory_positions) - 1 - pos
+    if (pos_val != ".") and ("." in memory_positions):
+        print(pos+1)
+        first_empty_pos = memory_positions.index(".")
+        memory_positions[first_empty_pos] = pos_val
+        memory_positions = memory_positions[:pos]
+    if not "." in memory_positions:
+        break
+p1 = 0
+for i, el in enumerate(memory_positions):
+    p1 += (i*int(el))
+print(p1)
---
+++ puzzle_18_soln.py
@@ -0,0 +1,3 @@
+# Read and process input
+with open("puzzle_17-18_input.txt", "r") as file:
+    input_data = file.read().strip()
