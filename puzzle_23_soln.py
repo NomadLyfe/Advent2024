d683d4f75c2fa4ac2cec1a44265dcbbbe123bc5b
Jeremy <jeremygarcia9400@gmail.com>
2025-01-12 17:33:48 -0500

    test

--- puzzle_23_soln.py
+++ puzzle_23_soln.py
@@ -3,4 +3,5 @@ with open("puzzle_23-24_input.txt", "r") as file:
     input_data_rows = file.read().strip().split('\n')
     data = [[char for char in row] for row in input_data_rows]
 
-print(data)
+for d in data:
+    print(d)
