b673fe638441ca725d3ee801069e85ba87c3b647
Jeremy <jeremygarcia9400@gmail.com>
2025-01-12 17:26:31 -0500

    setup for puzzles 23 and 24

---
+++ puzzle_23_soln.py
@@ -0,0 +1,6 @@
+# Read and process input
+with open("puzzle_23-24_input.txt", "r") as file:
+    input_data_rows = file.read().strip().split('\n')
+    data = [[char for char in row] for row in input_data_rows]
+
+print(data)
