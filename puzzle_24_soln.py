9e8c7e3ec8913c39407e935e3d1263c996fb6f1f
Jeremy Garcia <Jeremygarcia9400@gmail.com>
2025-01-14 00:22:50 -0500

    closer to solution, i am underestimating, but close!

--- puzzle_24_soln.py
+++ puzzle_24_soln.py
@@ -40,11 +40,27 @@ class Region():
                 if dy == 0:  # Horizontal segment
                     self.horizontal_sides.add((x, y, x + dx, y + dy))
 
+    def _count_unique_sides(self, sides):
+        # Filter out directly adjacent sides
+        sorted_sides = sorted(sides)
+        unique_sides = 0
+        seen_sides = []
+        print(sorted_sides)
+        for side in (sorted_sides):
+            if side not in seen_sides:
+                unique_sides += 1
+                seen_sides.append(side)
+            for side_2 in (sorted_sides):
+                if side != side_2 and (((side[0] == side_2[0]) and (side[2] == side_2[2]) and (abs(side[1] - side_2[1]) == 1)) or ((side[1] == side_2[1]) and side[3] == side_2[3] and (abs(side[0] - side_2[0]) == 1))):
+                    seen_sides.append(side_2)
+        return unique_sides
+
     def price(self):
-        # Calculate the number of unique sides
-        print(self.horizontal_sides)
-        print(self.vertical_sides)
-        total_sides = len(self.horizontal_sides) + len(self.vertical_sides)
+        # Count unique horizontal and vertical sides
+        unique_horizontals = self._count_unique_sides(self.horizontal_sides)
+        unique_verticals = self._count_unique_sides(self.vertical_sides)
+        print(unique_horizontals + unique_verticals)
+        total_sides = unique_horizontals + unique_verticals
         return self.area * total_sides
 
 # Initialize regions
@@ -59,6 +75,8 @@ for y, row in enumerate(grid):
             new_region.treverse_tree(x, y, garden_plot)
             p2 += new_region.price()
             regions[garden_plot].append(new_region)
+        break
+    break
 
 # Output the total price for Part 2
 print(p2)
