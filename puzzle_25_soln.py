bf8ec82e9847d3ae8b77a62c4e3ed35f4396248c
Jeremy Garcia <Jeremygarcia9400@gmail.com>
2025-01-16 22:27:31 -0500

    teased out the input into digestable data

--- puzzle_25_soln.py
+++ puzzle_25_soln.py
@@ -1,3 +1,8 @@
 # Read and process input
-with open("puzzle_23-24_input.txt", "r") as file:
-    input_data_rows = file.read().strip().split('\n')
+with open("puzzle_25-26_input.txt", "r") as file:
+    input_data_rows = file.read().strip().split("\n\n")
+games = [
+    {g.split(": ")[0]: g.split(": ")[1].split(", ") for g in game.split("\n")}
+    for game in input_data_rows
+]
+print(games)
